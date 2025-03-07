3477ec27717e17b6d36ce27014ccfb059d16b899
Purps <github@purps.ca>
2025-03-07 14:32:02 -0500

    First

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "ZaneyOS";
+
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nvf.url = "github:notashelf/nvf";
+    stylix.url = "github:danth/stylix/release-24.11";
+  };
+
+  outputs = {nixpkgs, ...} @ inputs: let
+    system = "x86_64-linux";
+    host = "nixos";
+    profile = "nvidia";
+    username = "purps";
+  in {
+    nixosConfigurations = {
+      nvidia = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit inputs;
+          inherit username;
+          inherit host;
+          inherit profile;
+        };
+        modules = [./profiles/nvidia];
+      };
+    };
+  };
+}
