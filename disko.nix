372205b0dc67047d2c90a2c958ac25871d460b09
Purps <github@purps.ca>
2025-03-07 14:33:30 -0500

    update

---
+++ disko.nix
@@ -0,0 +1,54 @@
+# sudo nix run github:nix-community/disko -- --mode disko disko.nix
+{
+  device ? "/dev/nvme0n1",
+  ...
+}: {
+  disko.devices = {
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "550M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = ["compress=zstd" "noatime"]; 
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = ["compress=zstd" "noatime"];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = ["compress=zstd" "noatime"];
+                };
+                "/root-blank" = {};
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
