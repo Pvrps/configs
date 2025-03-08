d51a64448f008f1e4c3e8cb6fe60dc5043049b26
Purps <github@purps.ca>
2025-03-08 16:12:08 -0500

    update

--- modules/shared/users/purps.nix
+++ modules/shared/users/purps.nix
@@ -9,6 +9,7 @@ in {
     {
       shell = pkgs.fish;
       isNormalUser = true;
+      hashedPassword = "$6$.iGquLq4H7sMMsGO$rJG04VDgKRJUwZ8OSCqsTJlZGYVrQgfjBwaIWWQ.tJy0Fnvj4jycRLLq5o7Etg30PJjmQ.glN.PX1gdX3qMmi1";
       extraGroups = ["wheel" "audio" "video" "plugdev" "libvirtd" "networkmanager"];
     }
   ];
